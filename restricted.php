7c48c625b735ba9237925676363353c01ec6aa80
MaximCS24 <72575365+MaximCS24@users.noreply.github.com>
2020-12-01 00:04:32 +0200

    Update restricted.php

--- restricted.php
+++ restricted.php
@@ -2,21 +2,10 @@
    session_start (); 
    require_once 'connection.php'; 
    $login = $_SESSION ['Login']; 
-   $password = $_SESSION ['Password']; 
-   if (!count($_POST)>0) {
-    $res = mysqli_query ($conn, "SELECT * FROM users WHERE login ='$login' and password = '$password'");
-    $row = mysqli_fetch_array($res);
-   if (!($_SESSION['id'] = $row['id'] && $_SESSION['login'] = $row['login'])) 
-    {
-      echo "Cпробуйте ще раз <br> "; 
-       echo "<html> 
-       <head><title>Information</title>
-       <a href='login.php'>login</a>
-       </head> 
-       </Html> ";
-    }
-    else{
-      echo "Добро пожаловать, ".$row['first_name']." ".$row['last_name'];
-    }
-  }
+   $password = $_SESSION ['Password'];
+   $res = mysqli_query ($conn, "SELECT * FROM users WHERE login ='$login' and password = '$password'");
+   $row = mysqli_fetch_array($res);
+   $_SESSION['first_name'] = $row['first_name'];
+	$_SESSION['last_name'] = $row['last_name'];
+   echo "Добро пожаловать, ".$_SESSION['first_name']." ".$_SESSION['last_name'];
   ?> 
